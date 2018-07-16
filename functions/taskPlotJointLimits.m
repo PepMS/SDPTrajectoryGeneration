c23f7994c5914bc8f303116df7fbba36a6b92ae7
Pep Martí Saumell <jmarti@bonadrone.com>
2018-07-16 13:04:52 +0200

    Plots finished

--- functions/taskPlotJointLimits.m
+++ functions/taskPlotJointLimits.m
@@ -3,15 +3,20 @@ function taskPlotJointLimits(robot, qT, lowBound, uppBound, colors)
     r2d = 180/pi;
     for i = 1:robot.n
         subplot(robot.n, 1, i)
+        y_min = min(qT(i, :)*r2d)-10;
+        y_max = max(qT(i, :)*r2d)+10;
         plot(qT(i, :)*r2d, 'Color', colors{1}, 'linewidth', 1.5, 'DisplayName', strcat('Joint ', i)); hold on
         if lowBound(i) ~= -1000
             plot(lowBound(i)*ones(1, length(qT)), 'LineStyle', ':', 'Color', colors{2}, 'linewidth', 1.5, 'DisplayName', 'Lower Bound');
+            y_min = lowBound(i) - 10;
         end
         if uppBound(i) ~= 1000
-            plot(uppBound(i)*ones(1, length(qT)), 'LineStyle', ':', 'Color', colors{2}, 'linewidth', 1.5, 'DisplayName', 'Upper Bound');
+            plot(uppBound(i)*ones(1, length(qT)), 'LineStyle', ':', 'Color', colors{3}, 'linewidth', 1.5, 'DisplayName', 'Upper Bound');
+            y_max = uppBound(i) + 10;
         end
-        xlim([0 length(qT)]);
+        xlim([0 length(qT)]); ylim([y_min, y_max]);
         legend
-        title(strcat('Limits for joint ', int2str(i)))
+        title(strcat('Limits for joint ', int2str(i)));
+        xlabel('samples'); ylabel('joint angle [deg]'); 
     end
 end
--- main.m
+++ main.m
@@ -186,7 +186,6 @@ disp(strcat(mtTitle, ' Solving Problem...'))
 % Defining nice colors
 colors = {[0,      0     , 1], 
           [0,      0.5   , 0], 
-          [0,      0     , 1], 
           [0,      0.4470, 0.7410],	          	
           [0.8500, 0.3250, 0.0980],	         
           [0.9290, 0.6940, 0.1250],	          	
